ff43d0d85720aca6583609f712348a0d17caa817
“asfarwaheed01” <asfarwaheedch123@gmail.com>
2024-10-14 13:16:33 +0500

    First Occurence in String

---
+++ PracticeQuestions/FirstOccurenceInString.js
@@ -0,0 +1,5 @@
+// Leetcode 28
+// Index of First Occurence in a String
+const findFirstOcc = (str, word) => str.indexOf(word);
+
+console.log(findFirstOcc("Asfar is a software Engineer", "Asfar"));
