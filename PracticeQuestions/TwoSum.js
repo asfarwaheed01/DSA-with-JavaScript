7835d00e7fec5a49b2d7c1f402d1969483da7536
Asfar Waheed <asfar.waheed@ginkgoretail.com>
2024-12-03 12:05:55 +0500

    Linkedlist

--- PracticeQuestions/TwoSum.js
+++ PracticeQuestions/TwoSum.js
@@ -17,3 +17,20 @@ arr1 = [1, 3, 7, 9, 2];
 target = 9;
 target1 = 11;
 console.log(twoSum(arr1, target1));
+
+// Better Solution using hash map
+
+const TwoSum = (nums, target) => {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let compliment = target - nums[i];
+    if (map.has(compliment)) {
+      return [map.get(compliment), i];
+    }
+    map.set(nums[i], i);
+  }
+  return 0;
+};
+
+const res = TwoSum([1, 3, 7, 9, 2], 11);
+console.log(res);
---
+++ linkedlist/LinkedList-2.js
@@ -0,0 +1,33 @@
+// Node
+class Node {
+  constructor(value) {
+    this.head = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+  pop() {}
+}
+
+const myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+// myLinkedList.push(3);
+// myLinkedList.push(4);
+console.log(myLinkedList);
