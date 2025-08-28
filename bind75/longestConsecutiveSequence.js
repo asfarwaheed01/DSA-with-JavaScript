df3995352028539f0c8f08052b48bd242cc3f978
Asfar Waheed <asfarwaheedch123@gmail.com>
2025-08-28 12:45:17 +0500

    longest consecutive sequence

--- bind75/longestConsecutiveSequence.js
+++ bind75/longestConsecutiveSequence.js
@@ -1,1 +1,5 @@
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
 var longestConsecutive = function (nums) {};
